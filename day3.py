6f82b6d34c980f46058e9bc3e24061d1cb6a6cf7
jrodwell9 <jrodwell9@icloud.com>
2022-12-06 08:57:53 +0000

    day 3 part 1

--- day3.py
+++ day3.py
@@ -3,14 +3,19 @@ from core.data_loader import load_data
 lower_letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")
 upper_letters = [i.upper() for i in lower_letters]
 
-priority_map = zip(lower_letters + upper_letters, list(range(1, 52)))
+priority_map = dict(zip(lower_letters + upper_letters, list(range(1, 53))))
 
 data = load_data("backpack_items.txt")
 
 parsed_data = data.split("\n")[:-1]
 
-for i in parsed_data:
-    split_point = len(i)/2 + 1
-    comp1 = i[:split_point]
-    comp2 = i[split_point:]
+total = 0
+
+for bag in parsed_data:
+    split_point = len(bag)//2
+    comp1 = {i for i in bag[:split_point]}
+    comp2 = {i for i in bag[split_point:]}
+    common = comp1.intersection(comp2)
+    total += sum(map(lambda x: priority_map[x], common))
     
+print(total)   
